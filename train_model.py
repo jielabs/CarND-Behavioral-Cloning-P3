bcf714aa95b7686ac9ebe1ac83b6838ee9436f5d
jielabs <cnjieyang@gmail.com>
2019-04-15 23:07:39 -0700

    add inception v3

--- train_model.py
+++ train_model.py
@@ -3,11 +3,13 @@
 
 from __future__ import print_function
 import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
+from keras.models import Sequential, Model
+from keras.layers import Input, Dense, Dropout, Flatten, Lambda
+from keras.layers import Cropping2D, Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.applications.inception_v3 import InceptionV3
 from sklearn.utils import shuffle
+from keras.backend import tf as ktf
+import tensorflow as tf
 import os
 import pandas as pd
 import numpy as np
@@ -54,41 +56,101 @@ def load_sim_data(data_dir, camera='center', shuffle_data=True, max_limit=0):
         print('Data shuffled')
     return x, y
 
-# data_folder = '../data/testrun'
-data_folder = '../data/data'
-x_train, y_train = load_sim_data(data_folder, 'center', shuffle_data=True, max_limit=0)
-x_test, y_test = load_sim_data(data_folder, 'left', shuffle_data=True, max_limit=640)
-print('Loaded', x_train.shape, y_train.shape)
+def get_training_data(data_folder = '../data/data'):
+    x_train, y_train = load_sim_data(data_folder, 'center', shuffle_data=True, max_limit=0)
+    x_test, y_test = load_sim_data(data_folder, 'left', shuffle_data=True, max_limit=640)
+    return x_train, y_train, x_test, y_test
 
-batch_size = 64
-num_epochs = 4
+def baseline():
+    batch_size = 64
+    num_epochs = 4
+    learning_rate = 0.001
 
-input_shape = x_train[0].shape
+    x_train, y_train, x_test, y_test = get_training_data()
+    input_shape = x_train[0].shape  # 160, 320, 3
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=input_shape))
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
 
-model.compile(loss='mse',
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['mae', 'acc'])
+    model.compile(loss='mse',
+                  optimizer=keras.optimizers.Nadam(lr=learning_rate),
+                  metrics=['mae', 'mse'])
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=num_epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-model_path = 'cnn-model.h5'
-model.save(model_path)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+    print(model.summary())
 
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=num_epochs,
+              verbose=1,
+              validation_split=0.1)
+    score = model.evaluate(x_test, y_test, verbose=0)
+    model_path = 'baseline-model.h5'
+    model.save(model_path)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+
+def resize_normalize(image):
+    from keras.backend import tf as ktf
+
+    resized = ktf.image.resize_images(image, (139, 139))
+    resized = resized / 255.0 - 0.5
+
+    return resized
+
+
+def inception3():
+    batch_size = 64
+    num_epochs = 2
+    learning_rate = 0.001
+    input_size = 139
+
+    x_train, y_train, x_test, y_test = get_training_data()
+    input_shape = x_train[0].shape  # 160, 320, 3
+
+    car_input = Input(shape=input_shape)
+    crop_input = Cropping2D(cropping=((50, 20), (0, 0)))(car_input)
+    resized_input = Lambda(resize_normalize)(crop_input)
+
+    # Using Inception with ImageNet pre-trained weights
+    inception = InceptionV3(weights='imagenet', include_top=False,
+                            input_shape=(input_size, input_size, 3))
+
+    inp = inception(resized_input)
+    out = GlobalAveragePooling2D()(inp)
+
+    fc1 = Dense(64, activation='relu')(out)
+    predictions = Dense(1)(fc1)
+
+    model = Model(inputs=car_input, outputs=predictions)
+
+    # Compile the model
+    model.compile(loss='mse',
+                  optimizer=keras.optimizers.Nadam(lr=learning_rate),
+                  metrics=['mae'])
+
+    print(model.summary())
+
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=num_epochs,
+              verbose=1,
+              validation_split=0.1)
+    score = model.evaluate(x_test, y_test, verbose=0)
+    model_path = 'inception3-model.h5'
+    model.save(model_path)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+if __name__ == '__main__':
+    inception3()
